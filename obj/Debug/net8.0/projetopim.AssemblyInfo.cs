e6259fe46d41ef6c8946f45c94988f21d92c855f
gutto <gutozupcraft@gmail.com>
2024-11-20 16:01:28 -0300

    alteracao

--- obj/Debug/net8.0/projetopim.AssemblyInfo.cs
+++ obj/Debug/net8.0/projetopim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetopim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af91fc52ea73c5170d9c34168f263629c937841")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetopim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetopim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
